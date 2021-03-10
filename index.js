a9e9e4683a905889c355f08239620032978399aa
Paul Rowan <prowan@novisci.com>
2021-03-10 13:57:41 -0500

    Update js files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TableCache = exports.addModifiers = exports.RowData = exports.compileTemplate = void 0;
+exports.TableCache = exports.addModifiers = exports.getTemplateValue = exports.RowData = exports.compileTemplate = void 0;
 var template_1 = require("./lib/template");
 Object.defineProperty(exports, "compileTemplate", { enumerable: true, get: function () { return template_1.compileTemplate; } });
 Object.defineProperty(exports, "RowData", { enumerable: true, get: function () { return template_1.RowData; } });
+Object.defineProperty(exports, "getTemplateValue", { enumerable: true, get: function () { return template_1.getTemplateValue; } });
 var modifiers_1 = require("./lib/modifiers");
 Object.defineProperty(exports, "addModifiers", { enumerable: true, get: function () { return modifiers_1.addModifiers; } });
 var tableCache_1 = require("./lib/tableCache");
