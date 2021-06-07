54755a29733d41e935ad3a536a4d7a5bd18a920e
Paul Rowan <prowan@novisci.com>
2021-06-07 16:15:55 -0400

    improve null handling in table lookups and NaN handling in numeric modifiers

--- lib/tableCache.ts
+++ lib/tableCache.ts
@@ -2,18 +2,19 @@ import * as path from 'path'
 import * as fs from 'fs'
 
 export interface ITableCache {
-  lookup (table: string, key: string): string
+  lookup (table: string, key: string): string | null
 }
 
+type LookupTable = { [key: string]: string | null }
 export class TableCache implements ITableCache {
   private _tablesPath
-  private _tables: { [key: string]: any } = {}
+  private _tables: { [key: string]: LookupTable | false } = {}
 
   constructor (tablesPath: string) {
     this._tablesPath = tablesPath
   }
 
-  lookup (table: string, key: string) {
+  lookup (table: string, key: string | null): string | null {
     if (typeof this._tables[table] === 'undefined') {
       console.info(`Loading table: ${table}`)
       const tablePath = path.join(this._tablesPath, `${table}.json`)
@@ -24,9 +25,23 @@ export class TableCache implements ITableCache {
         this._tables[table] = false // Disable further attempts to load table
       }
     }
-    // Return table value of key, or value of '' in the table, or value of key itself
-    return this._tables[table]
-      ? this._tables[table][key] || this._tables[table][''] || key
-      : key
+
+    // Lookup key in the cached table
+    if (this._tables[table] === false) {
+      // Table not found, all queries return null
+      return null
+    } else {
+      const tbl = this._tables[table] as LookupTable
+      if (key !== null)  {
+        const v = tbl[key]
+        if (typeof v !== undefined) {
+          // Table loaded and key exists, return table value
+          return v
+        }
+      }
+      // Return the default table value or null if none defined
+      const def = tbl['']
+      return typeof def !== 'undefined' ? def : null
+    }
   }
 }
